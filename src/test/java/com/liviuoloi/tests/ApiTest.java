07d113aff78d191db9d981384aa2db35a6cce31f
Oloi Liviu <liviu.oloi@yahoo.com>
2025-10-19 20:30:54 +0300

    Update ApiTest.java
    Am modificat @Test(description)

--- src/test/java/com/liviuoloi/tests/ApiTest.java
+++ src/test/java/com/liviuoloi/tests/ApiTest.java
@@ -32,7 +32,7 @@ public class ApiTest {
         }
     }
 
-    @Test(description = "Verifică preluarea corectă a unei postări după ID")
+    @Test(description = "Verifică preluarea corectă a unei postări după titlu")
     public void test_preluare_postare_dupa_titlu() {
         // ARRANGE
         String urlComplet = this.baseUrl + "/todos/1";
